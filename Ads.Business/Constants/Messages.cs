738b9af774396fefc855794cf13619b2e158054a
Muratcan Agiç <muratcanagic@hotmail.com>
2023-11-21 17:29:00 +0300

    feat: for Page model, Dal and Business's concrete and abstract classess created.

--- Ads.Business/Constants/Messages.cs
+++ Ads.Business/Constants/Messages.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ads.Business.Constants
+﻿namespace Ads.Business.Constants
 {
   public static class Messages
   {
     public static string CategoryAdded = "Kategori Eklendi";
     public static string ValidationResultNull = "Doğrulama işlemi gerçekleştirilemedi";
+    public static string PageAdded = "Sayfa Eklendi";
+    public static string PageDeleted = "Sayfa Silindi";
+    public static string ListEmpty = "Liste Boş";
+    public static string PageNotFound = "Sayfa bulunamadı";
+    public static string NoPage = "Sayfa yok";
   }
 }
---
+++ Ads.Dal/Abstract/IPageDal.cs
@@ -0,0 +1,9 @@
+﻿using Ads.Core.Dal.Abstract;
+using Ads.Entities.Concrete;
+
+namespace Ads.Dal.Abstract
+{
+  public interface IPageDal : IEntityRepository<Page>
+  {
+  }
+}

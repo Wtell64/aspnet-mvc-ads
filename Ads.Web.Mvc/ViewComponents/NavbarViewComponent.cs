b8fc992030a978d0b57ac324538413c9ec5309ec
Muratcan Agiç <muratcanagic@hotmail.com>
2023-12-01 16:36:44 +0300

    Task 2 has been completed. -Popular categories converted to viewCompononet. -Most 5 popular categories listed.

--- Ads.Web.Mvc/ViewComponents/NavbarViewComponent.cs
+++ Ads.Web.Mvc/ViewComponents/NavbarViewComponent.cs
@@ -5,35 +5,26 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Ads.Web.Mvc.ViewComponents
 {
-  public class NavbarViewComponent : ViewComponent
-  {
-    IPageService _pageService;
-    ICategoryService _categoryService;
-    public NavbarViewComponent(IPageService pageService, ICategoryService categoryService)
-    {
-      _pageService = pageService;
-      _categoryService = categoryService;
-    }
+	public class NavbarViewComponent : ViewComponent
+	{
+		IPageService _pageService;
+		ICategoryService _categoryService;
+		public NavbarViewComponent(IPageService pageService, ICategoryService categoryService)
+		{
+			_pageService = pageService;
+			_categoryService = categoryService;
+		}
 
-    public IViewComponentResult Invoke()
-    {
-      var pages = _pageService.GetList<Page>();
-      var categories = _categoryService.GetList<Category>();
+		public IViewComponentResult Invoke()
+		{
+			var pages = _pageService.GetList<Page>();
+			var categories = _categoryService.GetList<Category>();
 
-      //var navbarViewDto = new NavbarViewDto() { CategoryNames = new List<string>(), PageTitles = new List<string>() };
+			var navbarViewDto = new NavbarViewDto();
+			navbarViewDto.Categories = categories.Data;
+			navbarViewDto.Pages = pages.Data;
 
-      //foreach (var page in pages.Data)
-      //{
-      //  navbarViewDto.PageTitles.Add(page.Title);
-      //}
-      //foreach (var category in categories.Data)
-      //{
-      //  navbarViewDto.CategoryNames.Add(category.Name);
-      //}
-      var navbarViewDto = new NavbarViewDto();
-      navbarViewDto.Categories = categories.Data;
-      navbarViewDto.Pages = pages.Data;
-      return View(navbarViewDto);
-    }
-  }
+			return View(navbarViewDto);
+		}
+	}
 }

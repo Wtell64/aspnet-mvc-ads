5becbf4932a8203cd2ddd8582ed8e0da257ee31c
Muratcan Agiç <muratcanagic@hotmail.com>
2023-12-02 12:28:42 +0300

    feat: headline search converted to view component, form directed to advert controller.

--- Ads.Web.Mvc/Controllers/AdvertController.cs
+++ Ads.Web.Mvc/Controllers/AdvertController.cs
@@ -1,64 +1,62 @@
 ﻿using Ads.Business.Abstract;
-using Ads.Business.Dtos.Advert;
-using Ads.Business.Dtos.AdvertComment;
-using Ads.Business.Dtos.AdvertImage;
 using Ads.Entities.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Ads.Web.Mvc.Controllers
 {
- 
-  public class AdvertController : Controller
-  {
-    private readonly IAdvertService _advertService;
-    private readonly IAdvertCommentService _advertCommentService;
-    private readonly IAdvertImageService _advertImageService;
 
-    public AdvertController(IAdvertService advertService, IAdvertCommentService advertCommentService, IAdvertImageService advertImageService)
-        {
-      _advertService = advertService;
-      _advertCommentService = advertCommentService;
-      _advertImageService = advertImageService;
-    }
-        public IActionResult Search(string query, int page)
-    {
-      return View();
-    }
-    [Route("/advert/title-slug")]
-    public IActionResult Detail(int id)
-    {
-      return View();
-    }
+	public class AdvertController : Controller
+	{
+		private readonly IAdvertService _advertService;
+		private readonly IAdvertCommentService _advertCommentService;
+		private readonly IAdvertImageService _advertImageService;
+
+		public AdvertController(IAdvertService advertService, IAdvertCommentService advertCommentService, IAdvertImageService advertImageService)
+		{
+			_advertService = advertService;
+			_advertCommentService = advertCommentService;
+			_advertImageService = advertImageService;
+		}
+		[HttpPost]
+		public IActionResult Search(string query, int page)
+		{
+			return View();
+		}
+		[Route("/advert/title-slug")]
+		public IActionResult Detail(int id)
+		{
+			return View();
+		}
 
 
-    public IActionResult Deneme()
-    {
-      var deneme = new Advert() { Title = "Krem Reklami", Description = "Bu bir kremdir" };
-      _advertService.Add(deneme);
-      return View(); 
-    
-    }
-    public IActionResult Index()
-    
-    {
-      //var deneme = new AdvertViewDto() { Title = "Krem Reklami", Description = "Bu bir kremdir", UserId = 1 };
-      //_advertService.Add(deneme);
-      //_advertService.Save();
+		public IActionResult Deneme()
+		{
+			var deneme = new Advert() { Title = "Krem Reklami", Description = "Bu bir kremdir" };
+			_advertService.Add(deneme);
+			return View();
 
-      //var deneme = new AdvertImageViewDto() { AdvertId = 3, ImagePath = "deneme" };
-      //_advertImageService.Add(deneme);
-      //_advertImageService.Save();
+		}
+		public IActionResult Index()
 
-      //var deneme = new AdvertCommentViewDto() { AdvertId = 3, Comment = "deneme2", UserId = 1 };
-      //_advertCommentService.Add(deneme);
-      //_advertCommentService.Save();
+		{
+			//var deneme = new AdvertViewDto() { Title = "Krem Reklami", Description = "Bu bir kremdir", UserId = 1 };
+			//_advertService.Add(deneme);
+			//_advertService.Save();
 
-      //_advertCommentService.DeleteById(2);
-      //_advertCommentService.Save();
+			//var deneme = new AdvertImageViewDto() { AdvertId = 3, ImagePath = "deneme" };
+			//_advertImageService.Add(deneme);
+			//_advertImageService.Save();
 
-      _advertService.DeleteById(3);
-      _advertService.Save();
-      return View();
-    }
-  }
+			//var deneme = new AdvertCommentViewDto() { AdvertId = 3, Comment = "deneme2", UserId = 1 };
+			//_advertCommentService.Add(deneme);
+			//_advertCommentService.Save();
+
+			//_advertCommentService.DeleteById(2);
+			//_advertCommentService.Save();
+
+			_advertService.DeleteById(3);
+			_advertService.Save();
+			return View();
+		}
+	}
 }

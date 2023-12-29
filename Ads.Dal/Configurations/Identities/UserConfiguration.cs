1e661fdcf269423bf2baf250018af5ab8403d224
Muratcan Agiç <muratcanagic@hotmail.com>
2023-12-29 11:50:59 +0300

    advert detail comment submitting done!

--- Ads.Dal/Configurations/Identities/UserConfiguration.cs
+++ Ads.Dal/Configurations/Identities/UserConfiguration.cs
@@ -5,170 +5,170 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Ads.Dal.Configurations.Identities
 {
-	public class UserConfiguration : IEntityTypeConfiguration<AppUser>
-	{
-		public void Configure(EntityTypeBuilder<AppUser> builder)
-		{
-			builder.HasKey(u => u.Id);
+  public class UserConfiguration : IEntityTypeConfiguration<AppUser>
+  {
+    public void Configure(EntityTypeBuilder<AppUser> builder)
+    {
+      builder.HasKey(u => u.Id);
 
-			// Indexes for "normalized" username and email, to allow efficient lookups
-			builder.HasIndex(u => u.NormalizedUserName).HasName("UserNameIndex").IsUnique();
-			builder.HasIndex(u => u.NormalizedEmail).HasName("EmailIndex");
+      // Indexes for "normalized" username and email, to allow efficient lookups
+      builder.HasIndex(u => u.NormalizedUserName).HasName("UserNameIndex").IsUnique();
+      builder.HasIndex(u => u.NormalizedEmail).HasName("EmailIndex");
 
-			// Maps to the AspNetUsers table
-			builder.ToTable("AspNetUsers");
+      // Maps to the AspNetUsers table
+      builder.ToTable("AspNetUsers");
 
-			// A concurrency token for use with the optimistic concurrency checking
-			builder.Property(u => u.ConcurrencyStamp).IsConcurrencyToken();
+      // A concurrency token for use with the optimistic concurrency checking
+      builder.Property(u => u.ConcurrencyStamp).IsConcurrencyToken();
 
-			// Limit the size of columns to use efficient database types
-			builder.Property(u => u.UserName).HasMaxLength(100);
-			builder.Property(u => u.PhoneNumber).HasColumnType("varchar(20)");
-			builder.Property(u => u.NormalizedUserName).HasMaxLength(256);
-			builder.Property(u => u.Email).HasColumnType("varchar(200)");
-			builder.Property(u => u.NormalizedEmail).HasMaxLength(256);
+      // Limit the size of columns to use efficient database types
+      builder.Property(u => u.UserName).HasMaxLength(100);
+      builder.Property(u => u.PhoneNumber).HasColumnType("varchar(20)");
+      builder.Property(u => u.NormalizedUserName).HasMaxLength(256);
+      builder.Property(u => u.Email).HasColumnType("varchar(200)");
+      builder.Property(u => u.NormalizedEmail).HasMaxLength(256);
 
-			// The relationships between User and other entity types
-			// Note that these relationships are configured with no navigation properties
+      // The relationships between User and other entity types
+      // Note that these relationships are configured with no navigation properties
 
-			// Each User can have many UserClaims
-			builder.HasMany<AppUserClaim>().WithOne().HasForeignKey(uc => uc.UserId).IsRequired();
+      // Each User can have many UserClaims
+      builder.HasMany<AppUserClaim>().WithOne().HasForeignKey(uc => uc.UserId).IsRequired();
 
-			// Each User can have many UserLogins
-			builder.HasMany<AppUserLogin>().WithOne().HasForeignKey(ul => ul.UserId).IsRequired();
+      // Each User can have many UserLogins
+      builder.HasMany<AppUserLogin>().WithOne().HasForeignKey(ul => ul.UserId).IsRequired();
 
-			// Each User can have many UserTokens
-			builder.HasMany<AppUserToken>().WithOne().HasForeignKey(ut => ut.UserId).IsRequired();
+      // Each User can have many UserTokens
+      builder.HasMany<AppUserToken>().WithOne().HasForeignKey(ut => ut.UserId).IsRequired();
 
-			// Each User can have many entries in the UserRole join table
-			builder.HasMany<AppUserRole>().WithOne().HasForeignKey(ur => ur.UserId).IsRequired();
+      // Each User can have many entries in the UserRole join table
+      builder.HasMany<AppUserRole>().WithOne().HasForeignKey(ur => ur.UserId).IsRequired();
 
-			var superadmin = new AppUser
-			{
-				Id = 1,
-				UserName = "superadmin@gmail.com",
-				NormalizedUserName = "SUPERADMIN@GMAIL.COM",
-				Email = "superadmin@gmail.com",
-				NormalizedEmail = "SUPERADMIN@GMAIL.COM",
-				PhoneNumber = "+000000000",
-				FirstName = "SuperAdmin",
-				LastName = "SuperAdmin",
-				PhoneNumberConfirmed = true,
-				EmailConfirmed = true,
-				SecurityStamp = Guid.NewGuid().ToString(),
-				ImagePath = "deneme"
-			};
-			superadmin.PasswordHash = CreatePasswordHash(superadmin, "123456");
+      var superadmin = new AppUser
+      {
+        Id = 1,
+        UserName = "superadmin@gmail.com",
+        NormalizedUserName = "SUPERADMIN@GMAIL.COM",
+        Email = "superadmin@gmail.com",
+        NormalizedEmail = "SUPERADMIN@GMAIL.COM",
+        PhoneNumber = "+000000000",
+        FirstName = "SuperAdmin",
+        LastName = "SuperAdmin",
+        PhoneNumberConfirmed = true,
+        EmailConfirmed = true,
+        SecurityStamp = Guid.NewGuid().ToString(),
+        ImagePath = "deneme"
+      };
+      superadmin.PasswordHash = CreatePasswordHash(superadmin, "123456");
 
-			var admin = new AppUser
-			{
-				Id = 2,
-				UserName = "admin@gmail.com",
-				NormalizedUserName = "ADMIN@GMAIL.COM",
-				Email = "admin@gmail.com",
-				NormalizedEmail = "ADMIN@GMAIL.COM",
-				PhoneNumber = "+000000000",
-				FirstName = "Admin",
-				LastName = "Admin",
-				PhoneNumberConfirmed = false,
-				EmailConfirmed = true,
-				SecurityStamp = Guid.NewGuid().ToString(),
-				ImagePath = "deneme"
-			};
-			admin.PasswordHash = CreatePasswordHash(admin, "123456");
+      var admin = new AppUser
+      {
+        Id = 2,
+        UserName = "admin@gmail.com",
+        NormalizedUserName = "ADMIN@GMAIL.COM",
+        Email = "admin@gmail.com",
+        NormalizedEmail = "ADMIN@GMAIL.COM",
+        PhoneNumber = "+000000000",
+        FirstName = "Admin",
+        LastName = "Admin",
+        PhoneNumberConfirmed = false,
+        EmailConfirmed = true,
+        SecurityStamp = Guid.NewGuid().ToString(),
+        ImagePath = "deneme"
+      };
+      admin.PasswordHash = CreatePasswordHash(admin, "123456");
 
-			var aras = new AppUser
-			{
-				Id = 3,
-				UserName = "user1@gmail.com",
-				NormalizedUserName = "USER1@GMAIL.COM",
-				Email = "user1@gmail.com",
-				NormalizedEmail = "USER1@GMAIL.COM",
-				PhoneNumber = "+000000000",
-				FirstName = "Aras",
-				LastName = "Menteşe",
-				PhoneNumberConfirmed = false,
-				EmailConfirmed = true,
-				SecurityStamp = Guid.NewGuid().ToString(),
-				ImagePath = "deneme"
-			};
-			aras.PasswordHash = CreatePasswordHash(aras, "123456");
+      var aras = new AppUser
+      {
+        Id = 3,
+        UserName = "user1@gmail.com",
+        NormalizedUserName = "USER1@GMAIL.COM",
+        Email = "user1@gmail.com",
+        NormalizedEmail = "USER1@GMAIL.COM",
+        PhoneNumber = "+000000000",
+        FirstName = "Aras",
+        LastName = "Menteşe",
+        PhoneNumberConfirmed = false,
+        EmailConfirmed = true,
+        SecurityStamp = Guid.NewGuid().ToString(),
+        ImagePath = "deneme"
+      };
+      aras.PasswordHash = CreatePasswordHash(aras, "123456");
 
-			var elif = new AppUser
-			{
-				Id = 4,
-				UserName = "user2@gmail.com",
-				NormalizedUserName = "USER2@GMAIL.COM",
-				Email = "user2@gmail.com",
-				NormalizedEmail = "USER2@GMAIL.COM",
-				PhoneNumber = "+000000000",
-				FirstName = "Elif",
-				LastName = "Sakçı Tuncer",
-				PhoneNumberConfirmed = false,
-				EmailConfirmed = true,
-				SecurityStamp = Guid.NewGuid().ToString(),
-				ImagePath = "deneme"
-			};
-			elif.PasswordHash = CreatePasswordHash(elif, "123456");
+      var elif = new AppUser
+      {
+        Id = 4,
+        UserName = "user2@gmail.com",
+        NormalizedUserName = "USER2@GMAIL.COM",
+        Email = "user2@gmail.com",
+        NormalizedEmail = "USER2@GMAIL.COM",
+        PhoneNumber = "+000000000",
+        FirstName = "Elif",
+        LastName = "Sakçı Tuncer",
+        PhoneNumberConfirmed = false,
+        EmailConfirmed = true,
+        SecurityStamp = Guid.NewGuid().ToString(),
+        ImagePath = "deneme"
+      };
+      elif.PasswordHash = CreatePasswordHash(elif, "123456");
 
-			var ismail = new AppUser
-			{
-				Id = 5,
-				UserName = "user3@gmail.com",
-				NormalizedUserName = "USER3@GMAIL.COM",
-				Email = "user3@gmail.com",
-				NormalizedEmail = "USER3@GMAIL.COM",
-				PhoneNumber = "+000000000",
-				FirstName = "İsmail",
-				LastName = "Yücer",
-				PhoneNumberConfirmed = false,
-				EmailConfirmed = true,
-				SecurityStamp = Guid.NewGuid().ToString(),
-				ImagePath = "deneme"
-			};
-			ismail.PasswordHash = CreatePasswordHash(ismail, "123456");
+      var ismail = new AppUser
+      {
+        Id = 5,
+        UserName = "user3@gmail.com",
+        NormalizedUserName = "USER3@GMAIL.COM",
+        Email = "user3@gmail.com",
+        NormalizedEmail = "USER3@GMAIL.COM",
+        PhoneNumber = "+000000000",
+        FirstName = "İsmail",
+        LastName = "Yücer",
+        PhoneNumberConfirmed = false,
+        EmailConfirmed = true,
+        SecurityStamp = Guid.NewGuid().ToString(),
+        ImagePath = "deneme"
+      };
+      ismail.PasswordHash = CreatePasswordHash(ismail, "123456");
 
-			var muratcan = new AppUser
-			{
-				Id = 6,
-				UserName = "user4@gmail.com",
-				NormalizedUserName = "USER4@GMAIL.COM",
-				Email = "user4@gmail.com",
-				NormalizedEmail = "USER4@GMAIL.COM",
-				PhoneNumber = "+000000000",
-				FirstName = "Muratcan",
-				LastName = "Agıç",
-				PhoneNumberConfirmed = false,
-				EmailConfirmed = true,
-				SecurityStamp = Guid.NewGuid().ToString(),
-				ImagePath = "deneme"
-			};
-			muratcan.PasswordHash = CreatePasswordHash(muratcan, "123456");
+      var muratcan = new AppUser
+      {
+        Id = 6,
+        UserName = "user4@gmail.com",
+        NormalizedUserName = "USER4@GMAIL.COM",
+        Email = "user4@gmail.com",
+        NormalizedEmail = "USER4@GMAIL.COM",
+        PhoneNumber = "+000000000",
+        FirstName = "Muratcan",
+        LastName = "Agıç",
+        PhoneNumberConfirmed = false,
+        EmailConfirmed = true,
+        SecurityStamp = Guid.NewGuid().ToString(),
+        ImagePath = "deneme"
+      };
+      muratcan.PasswordHash = CreatePasswordHash(muratcan, "123456");
 
-			var ridvan = new AppUser
-			{
-				Id = 7,
-				UserName = "user5@gmail.com",
-				NormalizedUserName = "USER5@GMAIL.COM",
-				Email = "user5@gmail.com",
-				NormalizedEmail = "USER5@GMAIL.COM",
-				PhoneNumber = "+000000000",
-				FirstName = "Rıdvan",
-				LastName = "Kesken",
-				PhoneNumberConfirmed = false,
-				EmailConfirmed = true,
-				SecurityStamp = Guid.NewGuid().ToString(),
-				ImagePath = "deneme"
-			};
-			ridvan.PasswordHash = CreatePasswordHash(ridvan, "123456");
+      var ridvan = new AppUser
+      {
+        Id = 7,
+        UserName = "user5@gmail.com",
+        NormalizedUserName = "USER5@GMAIL.COM",
+        Email = "user5@gmail.com",
+        NormalizedEmail = "USER5@GMAIL.COM",
+        PhoneNumber = "+000000000",
+        FirstName = "Rıdvan",
+        LastName = "Kesken",
+        PhoneNumberConfirmed = false,
+        EmailConfirmed = true,
+        SecurityStamp = Guid.NewGuid().ToString(),
+        ImagePath = "deneme"
+      };
+      ridvan.PasswordHash = CreatePasswordHash(ridvan, "123456");
 
-			builder.HasData(superadmin, admin, aras, elif, ismail, muratcan, ridvan);
+      builder.HasData(superadmin, admin, aras, elif, ismail, muratcan, ridvan);
 
-		}
-		private string CreatePasswordHash(AppUser user, string password)
-		{
-			var passwordHasher = new PasswordHasher<AppUser>();
-			return passwordHasher.HashPassword(user, password);
-		}
-	}
+    }
+    private string CreatePasswordHash(AppUser user, string password)
+    {
+      var passwordHasher = new PasswordHasher<AppUser>();
+      return passwordHasher.HashPassword(user, password);
+    }
+  }
 }
--- Ads.Web.Mvc/Controllers/AdvertController.cs
+++ Ads.Web.Mvc/Controllers/AdvertController.cs
@@ -1,6 +1,8 @@
 using Ads.Business.Abstract;
 using Ads.Entities.Concrete;
 using Ads.Entities.Concrete.Enums;
+using Ads.Entities.Concrete.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.CodeAnalysis;
@@ -8,121 +10,142 @@ using Microsoft.CodeAnalysis;
 namespace Ads.Web.Mvc.Controllers
 {
 
-	public class AdvertController : Controller
-	{
-		private readonly IAdvertService _advertService;
-		private readonly IAdvertCommentService _advertCommentService;
-		private readonly IAdvertImageService _advertImageService;
+  public class AdvertController : Controller
+  {
+    private readonly IAdvertService _advertService;
+    private readonly IAdvertCommentService _advertCommentService;
+    private readonly IAdvertImageService _advertImageService;
+    private readonly UserManager<AppUser> _userManager;
 
-		public AdvertController(IAdvertService advertService, IAdvertCommentService advertCommentService, IAdvertImageService advertImageService)
-		{
-			_advertService = advertService;
-			_advertCommentService = advertCommentService;
-			_advertImageService = advertImageService;
-		}
-		public IActionResult Search(string query, string category, string location, int page = 1, decimal minPrice = 0, decimal maxPrice = 5000, int condition = 999)
-		{
-			var adverts = _advertService.GetList<Advert>(filter: a => a.Price >= minPrice && a.Price <= maxPrice, includeProperties: "SubcategoryAdverts.Subcategory.Category,User.Address.City,User.AdvertComments,AdvertImages").Data;
+    public AdvertController(IAdvertService advertService, IAdvertCommentService advertCommentService, IAdvertImageService advertImageService, UserManager<AppUser> userManager)
+    {
+      _advertService = advertService;
+      _advertCommentService = advertCommentService;
+      _advertImageService = advertImageService;
+      _userManager = userManager;
+    }
+    public IActionResult Search(string query, string category, string location, int page = 1, decimal minPrice = 0, decimal maxPrice = 5000, int condition = 999)
+    {
+      var adverts = _advertService.GetList<Advert>(filter: a => a.Price >= minPrice && a.Price <= maxPrice, includeProperties: "SubcategoryAdverts.Subcategory.Category,User.Address.City,User.AdvertComments,AdvertImages").Data;
 
-			if (condition != 999)
-			{
-				adverts = adverts.Where(a => a.ConditionEnum == (AdvertConditionEnum)condition);
-			}
+      if (condition != 999)
+      {
+        adverts = adverts.Where(a => a.ConditionEnum == (AdvertConditionEnum)condition);
+      }
 
-			if (!string.IsNullOrEmpty(query))
-			{
-				adverts = adverts.Where(a => a.Title.Contains(query) || a.Description.Contains(query));
-			}
+      if (!string.IsNullOrEmpty(query))
+      {
+        adverts = adverts.Where(a => a.Title.Contains(query) || a.Description.Contains(query));
+      }
 
-			if (!string.IsNullOrEmpty(category))
-			{
-				adverts = adverts.Where(a => a.SubcategoryAdverts.Any(ca => ca.Subcategory.Category.Name == category || ca.Subcategory.Name == category));
-			}
+      if (!string.IsNullOrEmpty(category))
+      {
+        adverts = adverts.Where(a => a.SubcategoryAdverts.Any(ca => ca.Subcategory.Category.Name == category || ca.Subcategory.Name == category));
+      }
 
-			if (!string.IsNullOrEmpty(location))
-			{
-				adverts = adverts.Where(a => a.User.Address != null && (a.User.Address.City.Name == location || a.User.Address.Country == location));
-			}
+      if (!string.IsNullOrEmpty(location))
+      {
+        adverts = adverts.Where(a => a.User.Address != null && (a.User.Address.City.Name == location || a.User.Address.Country == location));
+      }
 
-			var totalPostCount = adverts.Count();
-			var postCountPerPage = 9; //10
-			var pageCount = Math.Ceiling((double)totalPostCount / postCountPerPage);
-			if (page <= 0) page = 1;
-			if (page > pageCount) page = (int)pageCount;
+      var totalPostCount = adverts.Count();
+      var postCountPerPage = 9; //10
+      var pageCount = Math.Ceiling((double)totalPostCount / postCountPerPage);
+      if (page <= 0) page = 1;
+      if (page > pageCount) page = (int)pageCount;
 
-			ViewBag.PageCount = pageCount;
+      ViewBag.PageCount = pageCount;
 
-			//Setting up the viewbags for the data filtering on the sidebar
+      //Setting up the viewbags for the data filtering on the sidebar
 
-			var subcategoryCounts = adverts
-					.SelectMany(a => a.SubcategoryAdverts.Select(ca => ca.Subcategory.Category.Name))
-					.GroupBy(name => name)
-					.Select(group => new { Subcategory = group.Key, Count = group.Count() })
-					.ToList();
+      var subcategoryCounts = adverts
+          .SelectMany(a => a.SubcategoryAdverts.Select(ca => ca.Subcategory.Category.Name))
+          .GroupBy(name => name)
+          .Select(group => new { Subcategory = group.Key, Count = group.Count() })
+          .ToList();
 
-			ViewBag.Subcategories = subcategoryCounts;
+      ViewBag.Subcategories = subcategoryCounts;
 
-			var countryCounts = adverts
-				.Where(a => a.User != null && a.User.Address != null && a.User.Address.City != null)
-				.GroupBy(a => a.User.Address.City.Name)
-				.Select(group => new { Country = group.Key, Count = group.Count() })
-				.ToList();
+      var countryCounts = adverts
+        .Where(a => a.User != null && a.User.Address != null && a.User.Address.City != null)
+        .GroupBy(a => a.User.Address.City.Name)
+        .Select(group => new { Country = group.Key, Count = group.Count() })
+        .ToList();
 
-			ViewBag.Countries = countryCounts;
+      ViewBag.Countries = countryCounts;
 
-			var conditions = adverts
-					.SelectMany(a => a.SubcategoryAdverts.Select(ca => ca.Subcategory.Category.Name))
-					.GroupBy(name => name)
-					.Select(group => new { Subcategory = group.Key, Count = group.Count() })
-					.ToList();
+      var conditions = adverts
+          .SelectMany(a => a.SubcategoryAdverts.Select(ca => ca.Subcategory.Category.Name))
+          .GroupBy(name => name)
+          .Select(group => new { Subcategory = group.Key, Count = group.Count() })
+          .ToList();
 
-			ViewBag.ConditionEnumValues = Enum.GetValues(typeof(AdvertConditionEnum))
-																	.Cast<AdvertConditionEnum>()
-																	.Select(e => new SelectListItem
-																	{
-																		Value = ((int)e).ToString(),
-																		Text = e.ToString()
-																	})
-																	.ToList();
+      ViewBag.ConditionEnumValues = Enum.GetValues(typeof(AdvertConditionEnum))
+                                  .Cast<AdvertConditionEnum>()
+                                  .Select(e => new SelectListItem
+                                  {
+                                    Value = ((int)e).ToString(),
+                                    Text = e.ToString()
+                                  })
+                                  .ToList();
 
-			var advertsPageified = adverts
-			.Skip((page - 1) * postCountPerPage).Take(postCountPerPage);
+      var advertsPageified = adverts
+      .Skip((page - 1) * postCountPerPage).Take(postCountPerPage);
 
-			ViewBag.AdvertCount = totalPostCount;
-			ViewBag.Query = query;
-			ViewBag.Category = category;
-			ViewBag.Location = location;
-			ViewBag.MinPrice = minPrice;
-			ViewBag.MaxPrice = maxPrice;
-			ViewBag.Condition = condition;
-
-
-			return View(advertsPageified);
-
-		}
-		[HttpPost]
-		public IActionResult Search(string query, string location, string category)
-		{
-			return RedirectToAction("Search", new { query, location, category, page = 1 });
-		}
-
-		public IActionResult ChangePage(int page, string query, string category, string location, decimal minPrice, decimal maxPrice, int condition)
-		{
-			// Redirect to the Search action with the specified category
-			return RedirectToAction("Search", new { query, location, category, page, minPrice, maxPrice, condition });
-		}
-
-		[Route("/advert/title-slug")]
-		public IActionResult Detail(int id)
-		{
-			var advert = _advertService.Get<Advert>(a => a.Id == id, "User.Address.City,User.Address.District,AdvertComments.User,AdvertImages,SubcategoryAdverts.Subcategory");
-			advert.Data.ClickCount = advert.Data.ClickCount + 1;
-			_advertService.Update(advert.Data);
-			_advertService.Save();
-			return View(advert.Data);
-		}
+      ViewBag.AdvertCount = totalPostCount;
+      ViewBag.Query = query;
+      ViewBag.Category = category;
+      ViewBag.Location = location;
+      ViewBag.MinPrice = minPrice;
+      ViewBag.MaxPrice = maxPrice;
+      ViewBag.Condition = condition;
 
 
+      return View(advertsPageified);
 
-	}
+    }
+    [HttpPost]
+    public IActionResult Search(string query, string location, string category)
+    {
+      return RedirectToAction("Search", new { query, location, category, page = 1 });
+    }
+
+    public IActionResult ChangePage(int page, string query, string category, string location, decimal minPrice, decimal maxPrice, int condition)
+    {
+      // Redirect to the Search action with the specified category
+      return RedirectToAction("Search", new { query, location, category, page, minPrice, maxPrice, condition });
+    }
+
+    [Route("/advert/title-slug")]
+    public async Task<IActionResult> Detail(int id)
+    {
+      var advert = _advertService.Get<Advert>(a => a.Id == id, "User.Address.City,User.Address.District,AdvertComments.User,AdvertImages,SubcategoryAdverts.Subcategory");
+      var user = User.Identity.Name != null ? await _userManager.FindByEmailAsync(User.Identity.Name) : null;
+      ViewBag.userName = user != null ? user.FirstName + " " + user.LastName : "Misafir";
+      advert.Data.ClickCount = advert.Data.ClickCount + 1;
+      _advertService.Update(advert.Data);
+      _advertService.Save();
+      return View(advert.Data);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> SubmitComment(int advId, string review, int starCount)
+    {
+      if (starCount == 0) starCount = 3;
+
+      var user = User.Identity.Name != null ? await _userManager.FindByEmailAsync(User.Identity.Name) : null;
+      if (user == null) return RedirectToAction("Detail", "Advert", new { id = advId });
+
+      var advert = _advertService.Get<Advert>(a => a.Id == advId, "AdvertComments");
+      if (advert == null) return RedirectToAction("Detail", "Advert", new { id = advId });
+
+      advert.Data.AdvertComments.Add(new AdvertComment { UserId = user.Id, Comment = review, StarCount = starCount });
+      _advertService.Update(advert.Data);
+      await _advertService.SaveAsync();
+
+      return RedirectToAction("Detail", "Advert", new { id = advId });
+    }
+
+
+  }
 }
